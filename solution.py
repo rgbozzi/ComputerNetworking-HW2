af7657174779f46ff9ffdda4c89fec68daef3cb0
rgbozzi <90428416+rgbozzi@users.noreply.github.com>
2021-09-22 18:03:46 -0500

    Create solution.py
    initial script for TCP Server connections

---
+++ solution.py
@@ -0,0 +1,58 @@
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
+
+
+def webServer(port=13331):
+  serverSocket = socket(AF_INET, SOCK_STREAM)
+  #Prepare a server socket
+  serverSocket.bind(("", port))
+  serverSocket.listen(1)
+  
+
+  while True:
+    #Establish the connection
+    #print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    print('Ready to serve...')
+    
+    try:
+
+      try:
+        message = connectionSocket.recv(1024).decode()
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = #Fill in start     #Fill in end
+        
+        #Send one HTTP header line into socket.
+        #Fill in start
+        connectionSocket.send("http1.1 200 OK".encode())
+        #Fill in end
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+          connectionSocket.send(outputdata[i].encode())
+
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+      except IOError:
+        # Send response message for file not found (404)
+        #Fill in start
+        connectionSocket.send("http1.1 400 NOT FOUND".encode())
+        #Fill in end
+
+
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
+
+    except (ConnectionResetError, BrokenPipeError):
+      pass
+
+  serverSocket.close()
+  sys.exit()  # Terminate the program after sending the corresponding data
+
+if __name__ == "__main__":
+  webServer(13331)
