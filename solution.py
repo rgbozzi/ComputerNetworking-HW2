f4b02f0792472ef9c07e08849bf93dd7fdbeb39a
rgbozzi <90428416+rgbozzi@users.noreply.github.com>
2021-09-22 18:38:28 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,20 +2,16 @@
 from socket import *
 # In order to terminate the program
 import sys
-
-
 def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
   serverSocket.listen(1)
   
-
   while True:
     #Establish the connection
     #print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
-    
     try:
       try:
         message = connectionSocket.recv(2048).decode()
@@ -29,7 +25,6 @@ def webServer(port=13331):
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
           connectionSocket.send(outputdata[i].encode())
-
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
       except IOError:
